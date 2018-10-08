d0d36ca51edf0199826de8773ce270add63bcd8d
deep <deep.com>
2018-10-08 18:34:12 +0800

    改价基本功能完成》master

--- test/serve/localFile.js
+++ test/serve/localFile.js
@@ -10,12 +10,8 @@ var staticServe = require('./staticServe');
 const baseUrl = "E:\\ecs_self\\ecs\\src\\ecs\\static";
 
 http.createServer(function (req, res) {
-
     var pathObj = url.parse(req.url,true);
-
     var pathname = pathObj.pathname;
-
-
 
     if (pathname=='/') {
         let filepath = path.join(__dirname,"../static/change_price.html");
@@ -31,66 +27,96 @@ http.createServer(function (req, res) {
     }
     if (pathname=='/change_page_query') {
         let pagrurl = pathObj.query.urls;
-        let page_pathname = url.parse(pagrurl).pathname;
+        var page_pathname = url.parse(pagrurl).pathname;
+        let pageNameARR = page_pathname.split('/');
+        if (pageNameARR.length==3){
+            page_pathname+= '/' + pageNameARR[pageNameARR.length - 1]
+        }
+        var pageName = pageNameARR[pageNameARR.length - 1];
         let absolout_address = path.join(baseUrl,page_pathname);
         fs.readFile(absolout_address + '.ftl',function (err,data) {
             if (err){
                 console.log(err);
-                res.writeHead(404,{"Content-Type":"text/html; charset='utf-8'"});
+                res.writeHead(404,{"Content-Type":"application/json; charset='utf-8'"});
                 res.end('404');
             }
             let html = data.toString();
             let $ = cheerio.load(html);
-            let priceArr = [];
+            let ifwide = pageName.split('_').indexOf('wide');
 
-            let inps = $('.inp input');
-            for (let i = 0; i< inps.length; i++){
-                if (inps.eq(i).attr('data-price') != priceArr[i-1]){
-                    priceArr.push(inps.eq(i).attr('data-price'));
+            if (ifwide== -1){
+                let priceArr = [];
+                let inps = $('#radioCombox .inp input') || $('#radioCombox label input');
+                if (!inps){
+                    var endObj = {
+                        status:'1',
+                        msg:"模板5页面"
+                    }
+                }else{
+                    for (let i = 0; i< inps.length; i++){
+                        if (inps.eq(i).attr('data-price') != priceArr[i-1]){
+                            priceArr.push(inps.eq(i).attr('data-price'));
+                        }
+                    }
+                    if (priceArr.length==0){
+                        var endObj = {
+                            status:'0',
+                            msg:"模板一，需手动改成模板二"
+                        }
+                    }else{
+                        var endObj = {
+                            status:'1',
+                            endPrice:priceArr,
+                            msg:"模板2或模板3页面"
+                        }
+                    }
+                }
+            }else{
+                var endObj = {
+                    status:'1',
+                    msg:"宽屏页面"
                 }
             }
-            let endObj = {
-                msg:'1',
-                endPrice:priceArr,
-            }
-            let endString = JSON.stringify(endObj);
-
-            console.log(endPrice);
-
-            res.writeHead(200,{"Content-Type":"text/html; charset='utf-8'"});
-
+            endObj.local_url = "localhost:8000" + page_pathname + "?cf=localhost";
+            var endString = JSON.stringify(endObj);
+            res.writeHead(200,{"Content-Type":"application/json; charset='utf-8'"});
             res.end(endString);
         })
     }
     if (pathname=='/change_price_query') {
-        let price = pathObj.query.price;
+        var new_price = JSON.parse(pathObj.query.new_price);
+        var old_price = JSON.parse(pathObj.query.old_price);
         let pagrurl = pathObj.query.urls;
         let page_pathname = url.parse(pagrurl).pathname;
+
+        let pageNameARR = page_pathname.split('/');
+        if (pageNameARR.length==3){
+            page_pathname+= '/' + pageNameARR[pageNameARR.length - 1]
+        }
+        var pageName = pageNameARR[pageNameARR.length - 1];
         let absolout_address = path.join(baseUrl,page_pathname);
+
+        console.log(typeof new_price);
 
         fs.readFile(absolout_address + '.ftl',function (err,data) {
             if (err){
                 console.log(err);
                 return false;
             }
-            let html = data.toString();
-
-            console.log(html);
-
-            html = html.replace(/798/g,price);
-            // var reg = new RegExp(price,"g");
-            // html.replace(reg,price)
-
+            var html = data.toString();
+            for (let i = 0;i<new_price.length;i++){
+                var reg = new RegExp(old_price[i],"g");
+                html = html.replace(reg,new_price[i]);
+            }
             fs.writeFile(absolout_address + '.ftl',html,function (err,data) {
                 if (err){
                     console.log(err);
                     return false;
                 }
-                res.writeHead(200,{"Content-Type":"text/html; charset='utf-8'"});
+                res.writeHead(200,{"Content-Type":"application/json; charset='utf-8'"});
                 res.end('1');
             })
         })
     }
     staticServe.creatStaticServe(req,res,'../static');
-
 }).listen(8001);
